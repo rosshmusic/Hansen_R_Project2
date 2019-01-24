83ada0879f4e037ce6e81bfe3d5149d8a81f8651
Ross Hansen <rhansen@inthejourney.com>
2019-01-23 21:51:09 -0600

    Initial Push

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿# include <iostream>
+using System;
+
+class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int LENGTH = 100;
+            int WIDTH = 25;
+
+            Console.WriteLine("Hello World!");
+
+            cout << "BPCC";
+            cout << "6220 E Texas St.";
+            cout << "Bossier City, LA 71111";
+            cout << "318-678-6000";
+
+            cout << "The area of the building is " << LENGTH * WIDTH;
+            cout << "The perimeter of the building is " << 2 * LENGTH + 2 * WIDTH;
+        }
+    }
